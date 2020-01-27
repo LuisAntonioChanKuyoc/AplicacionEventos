05cf81a0405922130c5203691622dbb16cd1c0ac
LuisAntonioChanKuyoc <50585852+LuisAntonioChanKuyoc@users.noreply.github.com>
2020-01-27 16:59:29 -0600

    Formato tiempos

--- Aplicacion_Eventos/Eventos/FormatoTiempo.cs
+++ Aplicacion_Eventos/Eventos/FormatoTiempo.cs
@@ -13,7 +13,7 @@ namespace Aplicacion_Eventos.Eventos
             int tiempo = dtCompararTiempo.Days;
             string cFormatoTiempo = "";
 
-            double fTiempo =(double) tiempo / 30;
+            double fTiempo = (double)tiempo / 30;
 
             cFormatoTiempo = $"{Math.Truncate(fTiempo)}";
 
@@ -27,34 +27,59 @@ namespace Aplicacion_Eventos.Eventos
 
             //DateTime.TryParse(le, out DateTime dt);
 
-            string cMes = Math.Abs((int)fTiempo) == 1 ? $"{Math.Abs((int)fTiempo)} mes" : Math.Abs((int)fTiempo) > 0 ? $"{Math.Abs((int)fTiempo)} meses" : "";
-            string cDias="";
-            string cMinutos="";
-            string cHoras="";
-
+            //string cMes = Math.Abs((int)fTiempo) == 1 ? $"{Math.Abs((int)fTiempo)} mes" : Math.Abs((int)fTiempo) > 0 ? $"{Math.Abs((int)fTiempo)} meses" : "";
+            string cDias = "";
+            string cMinutos = "";
+            string cHoras = "";
+            string cMes = "";
             if (Math.Abs(fTiempo) > 0)
             {
-                 cDias = Math.Abs(dt.Day) == 1 ? $"{ Math.Abs(dt.Day)} día" : Math.Abs(dt.Day) > 1 ? $"{ Math.Abs(dt.Day)} días" : "";
-                 cMinutos = Math.Abs(dt.Minute) == 1 ? $"{Math.Abs(dt.Minute)} minuto" : Math.Abs(dt.Minute) > 1 ? $"{Math.Abs(dt.Minute)} minutos" : "";
-                 cHoras = Math.Abs(dt.Hour) == 1 ? $"{Math.Abs(dt.Hour)} hora" : Math.Abs(dt.Hour) > 1 ? $"{Math.Abs(dt.Hour)} horas" : "";
+                cDias = Math.Abs(dt.Day) == 1 ? $"{ Math.Abs(dt.Day)} día" : Math.Abs(dt.Day) > 1 ? $"{ Math.Abs(dt.Day)} días" : "";
+                cMinutos = Math.Abs(dt.Minute) == 1 ? $"{Math.Abs(dt.Minute)} minuto" : Math.Abs(dt.Minute) > 1 ? $"{Math.Abs(dt.Minute)} minutos" : "";
+                cHoras = Math.Abs(dt.Hour) == 1 ? $"{Math.Abs(dt.Hour)} hora" : Math.Abs(dt.Hour) > 1 ? $"{Math.Abs(dt.Hour)} horas" : "";
 
             }
             else
             {
 
             }
+
+
+
+
+
+
+
+            ////////////////////////////////////////
+
+            //double meses = dtCompararTiempo.TotalDays;
+            double meses = (double)tiempo / 30;
+
+
+            TimeSpan day = TimeSpan.FromDays(meses);
+            int iDias = day.Days;
+            int iHoras = day.Hours;
+            int iMinutos = day.Minutes;
+            ///////////////////////////////////////
+
+            cMes = Math.Abs((int)meses) == 1 ? $"{Math.Abs((int)meses)} mes" : Math.Abs((int)meses) > 0 ? $"{Math.Abs((int)meses)} meses" : "";
+
+            cDias = Math.Abs(iDias) == 1 ? $"{ Math.Abs(iDias)} día" : Math.Abs(iDias) > 1 ? $"{ Math.Abs(iDias)} días" : "";
+            cHoras = Math.Abs(iHoras) == 1 ? $"{Math.Abs(iHoras)} hora" : Math.Abs(iHoras) > 1 ? $"{Math.Abs(iHoras)} horas" : "";
+            cMinutos = Math.Abs(iMinutos) == 1 ? $"{Math.Abs(iMinutos)} minuto" : Math.Abs(iMinutos) > 1 ? $"{Math.Abs(iMinutos)} minutos" : "";
 
 
             string cTiempo;
-            if (dtFecha.Hour > 0)
-            {
-                cTiempo = $"{cMes} {cDias} {cHoras} {cMinutos}";
-            }
-            else
-            {
-                cTiempo = $"{cMes} {cDias} ";
+            //if (dtFecha.Hour > 0)
+            //{
+            //    cTiempo = $"{cMes} {cDias} {cHoras} {cMinutos}";
+            //}
+            //else
+            //{
+            //    cTiempo = $"{cMes} {cDias} ";
 
-            }
+            //}
+            cTiempo = $"{cMes} {cDias} {cHoras} {cMinutos}";
 
             return cTiempo.Trim();
         }
